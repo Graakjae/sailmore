bb1cae4e5844a4544d10d4b97ef0a3da500ae2bf
mikkelhau <mikkel.hau@hotmail.com>
2023-11-17 11:32:28 +0100

    mysql configuration

--- mysql.php
+++ mysql.php
@@ -1,17 +1,28 @@
 <?php
 // $env = parse_ini_file('.env');
 // $header = $env["HEADER"];
-$env = parse_ini_file('.env');
+// $env = parse_ini_file('.env');
 
 
-$hostname = $env["HOSTNAME"];
-$username = $env["USERNAME"];
-$password = $env["PASSWORD"];
-$database = $env["DATABASE"];
+// $hostname = $env["HOSTNAME"];
+// $username = $env["USERNAME"];
+// $password = $env["PASSWORD"];
+// $database = $env["DATABASE"];
 
-$mySQL = new mysqli($hostname, $username, $password, $database);
-if (!$mySQL) {
-  die("Could not connect to the MySQL server: " . mysqli_connect_error());
+// $mySQL = new mysqli($hostname, $username, $password, $database);
+// if (!$mySQL) {
+//   die("Could not connect to the MySQL server: " . mysqli_connect_error());
 
   
+// }
+
+$server = "mysql111.unoeuro.com";
+$username = "mikkelhau_dk";
+$password = "Vkm86etg";
+$database = "mikkelhau_dk_db";
+$mySQL = new mysqli($server, $username, $password, $database);
+if(!$mySQL) {
+die("Could not connect to the MySQL server: " . mysqli_connect_error());
 }
+
+?>
