dd18b7b234d2671e65bb2fb4e99bfe0d00ee6329
mikkelhau <mikkel.hau@hotmail.com>
2023-11-20 12:58:06 +0100

    Search-bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,12 +4,12 @@ function SearchBar() {
             <form action="api.php" method="post" class="search-form">
             <div>
                 <img src="" />
-                <input type="text" placeholder="Where would you like to sail?" />
+                <input type="text" name ="destination" placeholder="Where would you like to sail?" />
             </div>
             <img src="" />
             <div>
                 <img src="" />
-                <input type="date" placeholder="When do you plan to travel?" />
+                <input type="date" name="" placeholder="When do you plan to travel?" />
             </div>
             <button type="submit" class="search-button">
                 <img src="" />
