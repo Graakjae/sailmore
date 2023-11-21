9e88dc6da5ce9eb3ad4b261dea7a7026e583ba52
Kalleper2 <142982112+Kalleper2@users.noreply.github.com>
2023-11-21 11:27:46 +0100

    Filter basic function

--- app/TripsOverview/page.js
+++ app/TripsOverview/page.js
@@ -1,7 +1,19 @@
-import FilterTrips from "@/components/FilterTrips";
 
-export default function TripsOverview() {
+import FilterTrips from '@/components/FilterTrips';
+
+function TripsOverview() {
+  const tripList = [
+    { id: 1, price: 50 },
+    { id: 2, price: 120 },
+
+  ];
+
   return (
-    <FilterTrips />
-  )
+    <div>
+      <h1>Trips Overview</h1>
+      <FilterTrips tripList={tripList} />
+    </div>
+  );
 }
+
+export default TripsOverview;
