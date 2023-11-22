be8536717a0a972f6e2285ad9389cf75636338c5
mikkelhau <mikkel.hau@hotmail.com>
2023-11-22 13:30:13 +0100

    Cleanup + fixes + renaming

--- components/Trips.js
+++ components/Trips.js
@@ -10,7 +10,7 @@ const TripsListComponent = () => {
 
     const fetchData = async () => {
         try {
-            const response = await fetch('/backend/phpScripts/DatabaseTrips.php');
+            const response = await fetch('/backend/phpScripts/getTrips.php');
             const result = await response.json();
             setTrips(result);
         } catch (error) {
@@ -24,10 +24,10 @@ const TripsListComponent = () => {
             <div style={{ display: 'flex', flexDirection: 'row' }}>
                 {trips.map((trip, index) => (
                     <div key={index} style={{ margin: '10px', padding: '10px', border: '1px solid #ccc' }}>
+                        <img src={trip.img} />
                         <h2>{trip.title}</h2>
-                        <p><strong>Start Date:</strong> {trip.start_date}</p>
-                        <p><strong>End Date:</strong> {trip.end_date}</p>
-                        <p><strong>Price:</strong> {`${trip.price}$/day`}</p>
+                        <p><strong></strong>{trip.start_date} - {trip.end_date}</p>
+                        <p><strong></strong> {`${trip.price}€/day`}</p>
                     </div>
                 ))}
             </div>
