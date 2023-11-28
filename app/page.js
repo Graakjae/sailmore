15e02af9eb6963cf1f6a1b640a948a258d57bd8a
mikkelhau <mikkel.hau@hotmail.com>
2023-11-28 13:58:36 +0100

    Home Page trips fixed

--- app/page.js
+++ app/page.js
@@ -4,29 +4,72 @@ import "./styles.css";
 import Image from "next/image";
 import Footer from "/components/Footer";
 import HomePageHeader from "/components/HomePageHeader";
-import HomePageTrips from "/components/HomePageTrips";
 import LoggedIn from "@/components/loggedIn";
 import HomePageCrew from "@/components/HomePageCrew";
 import ExploreTripsButton from "@/components/buttons/ExploreTripsButton";
 import MoreCrewMembersButton from "@/components/buttons/MoreCrewMembersButton";
+import React, { useState, useEffect } from "react";
+import TripCard from "@/components/TripCard";
+
 function HomePage() {
-    return (
-        <div>
-            <Image src="/Båd_billede.png" alt="Site Logo" className="backgroundImage" width={1000} height={1000} />
-            <LoggedIn />
-            <br></br>
-            <HomePageHeader />
-            <br></br>
-            <HomePageTrips />
-            <br></br>
-            <ExploreTripsButton />
-            <br></br>
-            <HomePageCrew />
-            <br></br>
-            <MoreCrewMembersButton />
-            <br></br>
-            <Footer />
-        </div>
-    );
+  const [trips, setTrips] = useState([]);
+  const [filteredTrips, setFilteredTrips] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [targetArea, setTargetArea] = useState(null);
+  const [targetPrice, setTargetPrice] = useState(null);
+
+  async function fetchTrips() {
+    try {
+      const response = await fetch("/backend/phpScripts/getTripCards.php");
+      const result = await response.json();
+      setTrips(result);
+      setFilteredTrips(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTrips();
+  }, []);
+
+  return (
+    <div>
+      <Image
+        src="/Båd_billede.png"
+        alt="Site Logo"
+        className="backgroundImage"
+        width={1000}
+        height={1000}
+      />
+      <LoggedIn />
+      <br></br>
+      <HomePageHeader />
+      <br></br>
+      <h1>Trips List</h1>
+      {filteredTrips.slice(0, 8).map((trip, index) => (
+        <TripCard
+          title={trip.title}
+          startpoint={trip.startpoint}
+          destination={trip.destination}
+          start_date={trip.start_date}
+          end_date={trip.end_date}
+          price={trip.price}
+          img={trip.img}
+          key={index}
+          trip={trip}
+        />
+      ))}
+      <br></br>
+      <ExploreTripsButton />
+      <br></br>
+      <HomePageCrew />
+      <br></br>
+      <MoreCrewMembersButton />
+      <br></br>
+      <Footer />
+    </div>
+  );
 }
 export default HomePage;
--- components/TripCard.js
+++ components/TripCard.js
@@ -1,20 +1,34 @@
 "use client";
 
-const TripCard = ({title, startpoint, destination, start_date, end_date, price, img}) => {
-
-    return (
-        <div>
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-                    <div style={{ margin: '10px', padding: '10px', border: '1px solid #ccc' }}>
-                        <img src={`trip_img/${img}`} className='trip-img' />
-                        <h2 className='trip-title'>{title}</h2>
-                        <h3 className='trip-area'>{startpoint} to {destination}</h3>
-                        <p className='trip-dates'>{start_date} - {end_date}</p>
-                        <p className='trip-price'>{price}€/day</p>
-                    </div>
-            </div>
-        </div>
-    );
+const TripCard = ({
+  title,
+  startpoint,
+  destination,
+  start_date,
+  end_date,
+  price,
+  img,
+}) => {
+  return (
+    <div
+      style={{
+        margin: "10px",
+        padding: "10px",
+        border: "1px solid #ccc",
+        width: "300px",
+      }}
+    >
+      <img src={`trip_img/${img}`} className="trip-img" />
+      <h2 className="trip-title">{title}</h2>
+      <h3 className="trip-area">
+        {startpoint} to {destination}
+      </h3>
+      <p className="trip-dates">
+        {start_date} - {end_date}
+      </p>
+      <p className="trip-price">{price}€/day</p>
+    </div>
+  );
 };
 
 export default TripCard;
