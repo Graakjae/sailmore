f0058d9a9f9141a18557de625963e9b62146f467
Kalleper2 <142982112+Kalleper2@users.noreply.github.com>
2023-11-29 11:29:12 +0100

    Link trip

---
+++ app/Trip/[trip_id]/page.js
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+export default function TripPage() {
+    const [trip, setTrip] = useState({});
+    const params = useParams();
+
+    useEffect(() => {
+        const tripId = params.pk_id;
+
+        if (tripId) {
+            fetchTrip(tripId);
+        }
+    }, [params]);
+
+    const fetchTrip = async (tripId) => {
+        try {
+            const response = await fetch(`/backend/phpScripts/getTrips.php/${tripId}`);
+            const result = await response.json();
+
+            if (typeof result === "object" && result !== null) {
+                setTrip(result);
+            } else {
+                console.error("Unexpected data format:", result);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                <h3>Trip</h3>
+                {trip.title ? (
+                    <div>
+                        <h4>{trip.title}</h4>
+                        <p>Description: {trip.description}</p>
+                        <p>Start Point: {trip.startpoint}</p>
+                        <p>Destination: {trip.destination}</p>
+                        <p>Start Date: {trip.start_date}</p>
+                        <p>End Date: {trip.end_date}</p>
+                        <p>Price: {trip.price}</p>
+                        <p>Crew Capacity: {trip.crew_capacity}</p>
+                        <p>Rules: {trip.rules}</p>
+                        <div>
+                            <h5>Images</h5>
+                            {trip.img ? (
+                                <Image
+                                    src={`/trip_img/${trip.img}`}
+                                    alt={`Image of ${trip.title}`}
+                                    width={100}
+                                    height={100}
+                                />
+                            ) : (
+                                <p>No images for this trip</p>
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <p>No trip found</p>
+                )}
+            </div>
+        </div>
+    );
+}
--- backend/phpScripts/getTrips.php
+++ backend/phpScripts/getTrips.php
@@ -1,45 +1,31 @@
 <?php
+include "../../db/mysql.php";
 
-// Get root folder  
-$rootFolder = $_SERVER['DOCUMENT_ROOT'];
-$fullPath = $rootFolder . "/db/mysql.php";
+// Retrieve trip ID from the URL path
+$tripId = intval(basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
 
-include $fullPath;
+// Use prepared statements to prevent SQL injection
+$sql = "SELECT title, description, start_date, end_date, price, crew_capacity, rules, trip_img FROM trip_page WHERE pk_id = ?";
+$stmt = $mySQL->prepare($sql);
+$stmt->bind_param("i", $tripId);
+$stmt->execute();
+$result = $stmt->get_result();
 
-// Initialize an empty array to store the data
-$data = array();
-
-$sql = "SELECT * FROM trip";
-$result = $mySQL->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        // Append each item to the $data array
-        $data[] = array(
-            'title' => $row['title'],
-            'description' => $row['description'],
-            'startpoint' => $row['startpoint'],
-            'destination' => $row['destination'],
-            'start_date' => $row['start_date'],
-            'end_date' => $row['end_date'],
-            'price' => $row['price'],
-            'crew_capacity' => $row['crew_capacity'],
-            'captain_ID' => $row['captain_ID'],
-            'rules' => $row['rules'],
-            'trip_img' => $row['trip_img']
-        );
-    }
+if ($result === false) {
+    // Output any MySQL errors
+    echo json_encode(array('error' => 'MySQL Error: ' . $mySQL->error));
 } else {
-    // If no data found, you can set a specific message in the array
-    $data = array('message' => 'No data found');
-};
+    if ($result->num_rows > 0) {
+        $data = $result->fetch_assoc();
+    } else {
+        $data = array('message' => 'No data found');
+    }
+
+    // Encode the $data array as JSON and echo the result
+    header('Content-Type: application/json');
+    echo json_encode($data);
+}
 
 // Close the MySQL connection
 $mySQL->close();
-
-// Encode the $data array as JSON and echo the result
-header('Content-Type: application/json');
-$json_response = json_encode($data);
-echo $json_response;
-
 ?>
