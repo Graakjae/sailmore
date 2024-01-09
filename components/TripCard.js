f3d8c9985fadfa595094ba3a91f703a8acfe40a8
Frederik Graakjær <graakjaer.f@gmail.com>
2024-01-09 17:37:56 +0100

    trip images

--- components/TripCard.js
+++ components/TripCard.js
@@ -1,16 +1,19 @@
 "use client";
 import "../app/trips/tripsoverview.css";
+import Image from "next/image";
 
 const TripCard = ({ title, startpoint, destination, start_date, end_date, price, img }) => {
     return (
         <div className="trip-card">
-            <img
+            <Image
                 src={img}
                 className="trip-img"
                 alt="Trip Image"
                 onError={e => {
                     e.target.src = "/trip_img/defaultTripImage.jpg";
                 }}
+                width={300}
+                height={200}
             />
             <h2 className="trip-title-tripcard">{title}</h2>
             <h3 className="trip-area-tripcard">
